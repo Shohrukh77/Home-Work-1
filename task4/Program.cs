2064d9c5d59639e9c6575db1013ea5c073c91cdf
Shohrukh77 <y6prime2010@gmail.com>
2025-01-08 14:04:27 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿double a=Convert.ToDouble(Console.ReadLine());
+if(a<100)
+{
+    Console.WriteLine((a*5/100)+a);
+}
+else if (a>=100 && a<200)
+{
+    Console.WriteLine((a*7/100)+a);
+}
+else {
+   Console.WriteLine((a/10)+a);
+}
