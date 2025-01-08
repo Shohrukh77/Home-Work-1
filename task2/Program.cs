53264a61082def1f1ac8863adba013c377ac8a02
Shohrukh77 <y6prime2010@gmail.com>
2025-01-08 13:57:52 +0300

    add

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,12 +1,6 @@
-﻿int num = Convert.ToInt32(Console.ReadLine());
-int[] arr = new int[num];
-for (int i=1; i<=num; i++)
-{
-    arr[i] = Convert.ToInt32(Console.ReadLine());
-}
-int sum = 1;
-for (int i=0; i<num; i++)
-{
-    sum*=1;
+﻿int sum = 1;
+int[] num = {1, 2, 3, 4, 5};
+for (int i=1; i<=num.Length; i++) {
+    sum *= i;
 }
 System.Console.WriteLine("1*2*3*4*5 = " + sum);
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int max = -99999999;
+int[] num = {1, 2, 3, 4, 5};
+for (int i=0; i<num.Length; i++) {
+    if (i > max) {
+        max=i;
+    }
+}
+System.Console.WriteLine(max);
