1febf9ce5cb5f331238b73993cffece80841253b
Shohrukh77 <y6prime2010@gmail.com>
2025-01-08 13:47:18 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿double num=Convert.ToDouble(Console.ReadLine());
+if(num<100)
+{
+    Console.WriteLine((num*5/100)+num);
+}
+else if (num>=100 && num<200)
+{
+    Console.WriteLine((num*7/100)+num);
+}
+else {
+   Console.WriteLine((num/10)+num);
+}
---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[num];
+for (int i=1; i<=num; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+int sum = 1;
+for (int i=0; i<num; i++)
+{
+    sum*=1;
+}
+System.Console.WriteLine("1*2*3*4*5 = " + sum);
